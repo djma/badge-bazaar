70348dbe1e671b4e487ff260edf83cdb577a3c5a
David Ma <ma.davidj@gmail.com>
2023-08-04 16:06:04 -0700

    f

--- build.ts
+++ build.ts
@@ -1,9 +1,12 @@
 import { build } from "esbuild";
 import { polyfillNode } from "esbuild-plugin-polyfill-node";
 
+const isProd = process.env.NODE_ENV === "prod";
+
 build({
   entryPoints: ["src/web/index.tsx", "src/web/popup.tsx"],
   bundle: true,
   outdir: "www/dist",
   plugins: [polyfillNode()],
+  minify: isProd,
 });
