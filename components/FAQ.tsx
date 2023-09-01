9e53bb48e44f61a13656dba6c177e0b527a66370
David Ma <ma.davidj@gmail.com>
2023-09-01 14:13:13 -0700

    acknowledgements

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,7 +1,7 @@
 export function FAQ() {
   return (
     <div>
-      <h1>What is Badge Bazaar</h1>
+      <h1>What is Badge Bazaar?</h1>
       <p>
         Badge Bazaar is a zero-knowledge proof demo where users can post
         messages anonymously and have claims attached to the messages. That is,
@@ -10,6 +10,36 @@ export function FAQ() {
           <li>Claim and prove they are a whale.</li>
           <li>Keep their address private, even from the server.</li>
         </ul>
+        This demo was made possible by:
+        <ul>
+          <li>
+            <a href="https://twitter.com/0xPARC">@0xPARC</a> and{" "}
+            <a href="https://twitter.com/PrivacyScaling">@PrivacyScaling</a> for
+            ZK education, applied research, and upstream dependencies.
+          </li>
+          <li>
+            <a href="https://twitter.com/personae_labs">@personae_labs</a> for
+            their{" "}
+            <a href="https://github.com/personaelabs/spartan-ecdsa">
+              implementation
+            </a>{" "}
+            of a fast ECDSA group membership proof.
+          </li>
+          <li>
+            <a href="https://twitter.com/DeBankDeFi">@DeBankDeFi</a> for having
+            the best DeFi net worth tracker, and for making the{" "}
+            <a href="https://github.com/DeBankDeFi/web3-data">data</a>{" "}
+            available.
+          </li>
+          <li>
+            And of course, thanks to{" "}
+            <a href="https://twitter.com/alliancedao">@alliancedao</a> for
+            allowing time to be spent on this.
+          </li>
+        </ul>
+        <a href="https://alliance.xyz/apply">Alliance</a> is always looking for
+        founders who bring frontier ideas to life. Apply{" "}
+        <a href="https://alliance.xyz/apply">here</a>.
       </p>
       <h1>How does it work?</h1>
       <p>
